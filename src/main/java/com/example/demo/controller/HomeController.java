ffdb1f5449c978b9c1046a5663cc1ae6c5ea26bd
gongchao_vendor <gongchao_vendor@sensetime.com>
2019-02-20 22:08:16 +0800

    init

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController{
+
+    @GetMapping("/test")
+    public String Test(){
+        return "dsdsadsd";
+    }
+    @RequestMapping("/hello")
+    public String helloHtml(HashMap<String, Object> map) {
+        map.put("hello", "欢迎进入HTML页面");
+        return "/index";
+    }
+}
